37240de9d3182eb987f9da5af081f8bad7f28f05
oygc <362735156@qq.com>
2018-04-11 11:47:10 +0800

    路由配置完成
    添加了angular-ui-router

---
+++ Project/controller/fs.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2018/4/11.
+ */
+myApp.controller("fsCtrl", function ($scope) {
+
+}).controller("homeCtrl", function ($scope) {
+
+})
+;
--- Project/js/app.js
+++ Project/js/app.js
@@ -2,3 +2,24 @@
  * Created by Administrator on 2018/4/11.
  */
 var myApp = angular.module("myApp",["ui.router"]);
+
+myApp.config(["$stateProvider", function ($stateProvider,$urlRouterProvider) {
+
+    $stateProvider
+        .state("fs", { //导航用的名字，如<a ui-sref="login">login</a>里的login
+            url: '/fs',    //访问路径
+            templateUrl: 'template/baidubaike.html',
+            controller:'fsCtrl'
+        })
+        .state("second", {
+            url: '/bookStore',
+            templateUrl: 'template/bookStore.html',
+            controller: 'bookCtrl'
+        })
+        .state("home",{
+            url:'',
+            templateUrl:'template/home.html',
+            controller:'homeCtrl'
+        });
+
+}]);
